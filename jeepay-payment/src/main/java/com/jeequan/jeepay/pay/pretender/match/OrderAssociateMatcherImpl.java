34ef2f22dc6e63909e269d2ae4e38a1225e3ba7a
axl <heizi857857@163.com>
2021-10-04 20:30:57 +0800

    [fix] 1.fix bug

--- jeepay-payment/src/main/java/com/jeequan/jeepay/pay/pretender/match/OrderAssociateMatcherImpl.java
+++ jeepay-payment/src/main/java/com/jeequan/jeepay/pay/pretender/match/OrderAssociateMatcherImpl.java
@@ -1,6 +1,5 @@
 package com.jeequan.jeepay.pay.pretender.match;
 
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
