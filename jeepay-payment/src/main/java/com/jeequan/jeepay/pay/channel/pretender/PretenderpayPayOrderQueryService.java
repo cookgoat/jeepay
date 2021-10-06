8e7beeec8b6e14457def9a9e37d762c94bfd71f0
axl <heizi857857@163.com>
2021-10-06 14:55:10 +0800

    [add]

--- jeepay-payment/src/main/java/com/jeequan/jeepay/pay/channel/pretender/PretenderpayPayOrderQueryService.java
+++ jeepay-payment/src/main/java/com/jeequan/jeepay/pay/channel/pretender/PretenderpayPayOrderQueryService.java
@@ -119,7 +119,7 @@ public class PretenderpayPayOrderQueryService implements IPayOrderQueryService {
 
   private boolean isExpire(ResellerOrder resellerOrder) {
     if (resellerOrder == null || resellerOrder.getGmtPayingStart() == null) {
-      return false;
+      return true;
     }
     return DateUtil.getDistanceMinute(resellerOrder.getGmtPayingStart(), new Date())
         > sysConfigService.getDBApplicationConfig().getOrderMaxExpireMin();
