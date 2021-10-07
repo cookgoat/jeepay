b3571bd3892e4d539a4233a2a5f80815e70b01d2
axl <heizi857857@163.com>
2021-10-07 13:08:01 +0800

    [fix] 1.fix the query timing minute

--- jeepay-payment/src/main/java/com/jeequan/jeepay/pay/task/PayOrderReissueTask.java
+++ jeepay-payment/src/main/java/com/jeequan/jeepay/pay/task/PayOrderReissueTask.java
@@ -49,7 +49,7 @@ public class PayOrderReissueTask {
     public void start() {
 
         //当前时间 减去1分钟。
-        Date offsetDate = DateUtil.offsetMinute(new Date(), -4);
+        Date offsetDate = DateUtil.offsetMinute(new Date(), -2);
 
         //查询条件： 支付中的订单 & （ 订单创建时间 + 1分钟 >= 当前时间 ）
         LambdaQueryWrapper<PayOrder> lambdaQueryWrapper = PayOrder.gw().eq(PayOrder::getState, PayOrder.STATE_ING).le(PayOrder::getCreatedAt, offsetDate);
